a0071ec7ffc4ad62bfb09c624bcb3e46683be712
Ginna Mora <higinnamora@gmail.com>
2021-06-23 10:58:39 -0500

    Clase: Que se implemento en ecmascript 8

---
+++ es7/ecmascript7.js
@@ -0,0 +1,18 @@
+// Esto es lo que se implemento en ecmascript 8
+
+// Includes Nos permite saber si hay un elemento dentro de este
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+  console.log('Si se encuentra el valor 7');
+} else {
+  console.log('No se encuentra')
+}
+
+// Elevar a la potencia con dos **
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result)
